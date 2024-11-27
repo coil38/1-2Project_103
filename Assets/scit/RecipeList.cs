7b708cff525646b85545d92c408f8f67872f4304
coil38 <imcoco3838@naver.com>
2024-11-27 11:13:03 +0900

    [feat] 9강 BuildingType.cs에서 레시피 스텟  추가중(9강 스텟하강 함수 작성 까지함(이부분에서 오류뜸))부분 이어서 후 해야함

---
+++ Assets/scit/RecipeList.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeList : MonoBehaviour
+{
+    /*
+     public CraftingRecipe
+    {
+        itemName = "¾ßÃ¤½ºÆ©"
+        resultltem = 
+    
+     */
+}
